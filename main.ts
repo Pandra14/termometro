7416c513e6a42e6ae5ab0f4d1fcabbf71218be43
Pandra14 <130453008+Pandra14@users.noreply.github.com>
2023-05-30 12:15:30 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        radio.sendValue("temperatura", input.temperature())
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(1.8 * input.temperature() + 32)
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "temperatura") {
+        basic.showNumber(value)
+    }
+})
+basic.forever(function () {
+    radio.setGroup(58545256)
+})
